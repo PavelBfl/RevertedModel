6c545962b9ce8c796eb25387e099d1e6efa2c1e9
PavelBfl <33580861+PavelBfl@users.noreply.github.com>
2020-01-26 19:57:59 +0500

    + Реализован базовый клас тестирования отменяемого словаря

---
+++ RevertedModelTests/Collections/CommandedDictionaryTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RevertedModel.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevertedModel.Collections.Tests
+{
+	[TestClass()]
+	public class CommandedDictionaryTests
+	{
+		[TestMethod()]
+		public void CommandedDictionaryTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void ContainsKeyTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void AddTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void TryGetValueTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void ClearTest()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void RemoveTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
